bdb4235c252e1eee613a1cbd2e9c1298059d179c
houckontape <43896496+houckontape@users.noreply.github.com>
2022-05-14 01:16:00 +0200

    create entity for post and edit first post on homePage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,18 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PostHomeRepository;
+use Twig\Environment;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Environment $twig,PostHomeRepository $post): Response
     {
         // recuperation des info de configuration_Page
-
         // recuperation des info de Post_Home
+        $posts= $post->findAll();
+         return new Response($twig->render('home/index.html.twig', ['posts'=>$posts]));
         
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
     }
 }
---
+++ src/Entity/Role.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RoleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RoleRepository::class)]
+class Role
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $value;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
